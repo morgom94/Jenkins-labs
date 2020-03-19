dcea89c645d04c5a35eb16dee8096b0d76647215
morgom94 <58965348+morgom94@users.noreply.github.com>
2020-03-19 14:34:34 -0700

    Update Example.groovy

--- vars/Example.groovy
+++ vars/Example.groovy
@@ -1,3 +1,7 @@
 def call(String name = 'Erick Morales', String job = 'Softtek'){
+  Example([
+      branches: [[name: '*/master']], 
+      userRemoteConfigs: [[url: 'https://github.com/morgom94/Jenkins-labs.git']]
+  ])
   echo "Hello, my names is ${name} and I work in ${job}"
 }
