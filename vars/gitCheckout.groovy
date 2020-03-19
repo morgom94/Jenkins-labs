ac501b2bf6b6d979d8ae6e33b207234933c33a7f
morgom94 <58965348+morgom94@users.noreply.github.com>
2020-03-19 15:18:06 -0700

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -7,6 +7,15 @@ def call(Map stageParams, String name = 'softtekian', Integer age= 25) {
         submoduleCfg: [], 
         userRemoteConfigs: [[url: 'https://github.com/morgom94/Jenkins-labs.git']]
     ])
+    build job: 
+        'Pipe', 
+        parameters: [booleanParam(name: 'userFlag', value: true), 
+                     string(name: 'CHOICE', value: 'US-EAST-1'), 
+                     text(name: 'TEXT', value: '''Hello there'''), 
+                     password(description: 'A secret password', name: 'PASSWORD', value: <object of type hudson.util.Secret>), 
+                     string(name: 'STRING', value: 'Library example')
+                    ]
+    
     echo "Hello, ${name} "
     echo "and I'm ${age} years old."
 }
